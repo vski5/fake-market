9c03ecbdb8f68cb0338742462a5f31e435aa9a60
vski5 <2643380185@qq.com>
2022-11-01 10:42:42 +0000

    测试MySQL的本地连接和初始化文件

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/ini.v1" //注意引用的是指定版本号V1 "gopkg.in/ini.v1"
+)
 
 func main() {
-	fmt.Println("HELLO,WORLD")
+	r := gin.Default()
+	//读取.ini里面的数据库配置
+	config, err := ini.Load("./conf/app.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+
+	ginPort := config.Section("app").Key("port").String()
+	r.Run(ginPort)
 }
