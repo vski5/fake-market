50133d8b4af4610cc6510c46fd3b96ecf6a7fad1
vski5 <2643380185@qq.com>
2022-11-01 14:08:26 +0000

    静态服务路径，控制器与路由常见函数

--- controllers/admin/adminController.go
+++ controllers/admin/adminController.go
@@ -1,1 +1,16 @@
 package admin
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type AdminController struct{}
+
+func (a AdminController) Add(c *gin.Context) {
+	c.HTML(200, "html1/html1.html", gin.H{})
+}
+func (a AdminController) Back(c *gin.Context) {
+	c.String(http.StatusOK, "test2")
+}
--- routers/adminRouter.go
+++ routers/adminRouter.go
@@ -1,1 +1,15 @@
 package routers
+
+import (
+	"fake-market/controllers/admin"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AdminRouterInit(r *gin.Engine) {
+	adminRouters := r.Group("admin")
+	{
+		adminRouters.GET("/test1", admin.AdminController{}.Add)
+		adminRouters.GET("/test2", admin.AdminController{}.Back)
+	}
+}
