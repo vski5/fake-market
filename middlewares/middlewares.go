388c4da269d29a69b24967d00074f3179399b1dd
vski5 <2643380185@qq.com>
2022-11-03 07:40:16 +0000

    判断用户是否登录的鉴权middleware（记得配置到路由组里面），但是只写了一个架子

--- middlewares/middlewares.go
+++
@@ -1,3 +0,0 @@
-package middlewares
-
-//用于鉴权，每次路由请求之后都会用到的中间件
--- routers/adminRouter.go
+++ routers/adminRouter.go
@@ -2,12 +2,13 @@ package routers
 
 import (
 	"fake-market/controllers/admin"
+	"fake-market/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AdminRouterInit(r *gin.Engine) {
-	adminRouters := r.Group("admin")
+	adminRouters := r.Group("admin", middlewares.InitMiddleware)
 	{
 		adminRouters.GET("/login", admin.AdminController{}.Login)
 		adminRouters.POST("/dologin", admin.AdminController{}.Dolog)
