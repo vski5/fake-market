3686ba16e98af2d1e33a2d390e92b5501ce775f5
vski5 <2643380185@qq.com>
2022-11-03 03:21:26 +0000

    配置静态资源 加载HTML资源

--- main.go
+++ main.go
@@ -11,6 +11,17 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	//加载HTML资源
+	r.LoadHTMLGlob("templates/**/**/*")
+	//配置静态资源   第一个参数表示路由, 第二个参数表示映射的目录
+	r.Static("/static", "./static")
+
+	//自定义模板函数  注意要把这个函数放在加载模板前
+	/* r.SetFuncMap(template.FuncMap{
+		"函数名": 赋值给函数名的函数（不加括号）,
+	}) */
+
 	//读取.ini里面的数据库配置
 	config, err := ini.Load("./conf/app.ini")
 	if err != nil {
