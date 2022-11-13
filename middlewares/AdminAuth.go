4bca0a47e6959d72729a15d8a0e5b0a483e23344
vski5 <2643380185@qq.com>
2022-11-13 15:39:36 +0000

    加了一个判断，在无cookie的情况下重定向到登录界面

--- middlewares/AdminAuth.go
+++ middlewares/AdminAuth.go
@@ -15,38 +15,42 @@ func InitAdminAuthMiddleware(c *gin.Context /* , username string */) {
 	pathname := strings.Split(c.Request.URL.String(), "?")[0] //获取 Url 路径去掉 Get 传值
 	//获取值【我需要一种在两个函数间传递值的方法】
 	/* admincUsername := c.PostForm("username") */
-	cookie111, _ := c.Request.Cookie("admin_cookie")
-	userinfo := models.CookieRedisStore{}.Get(cookie111.Value)
-	/* userinfo := models.CookieRedisStore{}.Get("admin") */
-	//先类型断言判断是否为string，确定之后才能进行下一步
-	//类型断言
-	userinfoStr, ok := userinfo.(string)
-	if ok {
-		if len(userinfoStr) > 0 {
-			var u []models.Manager
+	cookie111, err := c.Request.Cookie("admin_cookie")
+	if err == nil {
+		c.Redirect(302, "admin/login")
+	} else {
+		userinfo := models.CookieRedisStore{}.Get(cookie111.Value)
 
-			json.Unmarshal([]byte(userinfoStr), &u)
+		/* userinfo := models.CookieRedisStore{}.Get("admin") */
+		//先类型断言判断是否为string，确定之后才能进行下一步
+		//类型断言
+		userinfoStr, ok := userinfo.(string)
+		if ok {
+			if len(userinfoStr) > 0 {
+				var u []models.Manager
 
-			//如果验证成功
-			if len(u) > 0 && u[0].Username != "" {
+				json.Unmarshal([]byte(userinfoStr), &u)
 
+				//如果验证成功
+				if len(u) > 0 && u[0].Username != "" {
+
+					if pathname != "/admin/login" && pathname != "/admin/doLogin" && pathname != "/admin/captcha" {
+						c.Redirect(302, "/admin/login")
+					}
+
+					fmt.Println("成功跳过验证登录的中间件")
+				}
+			} else { //如果验证失败
 				if pathname != "/admin/login" && pathname != "/admin/doLogin" && pathname != "/admin/captcha" {
 					c.Redirect(302, "/admin/login")
 				}
+			}
 
-				fmt.Println("成功跳过验证登录的中间件")
-			}
-		} else { //如果验证失败
-			if pathname != "/admin/login" && pathname != "/admin/doLogin" && pathname != "/admin/captcha" {
-				c.Redirect(302, "/admin/login")
-			}
+		} else {
+			c.HTML(200, "admin/public/error.html", gin.H{
+				"gotourl": "用户名或者密码异常",
+				"message": "/admin/login",
+			})
 		}
-
-	} else {
-		c.HTML(200, "admin/public/error.html", gin.H{
-			"gotourl": "用户名或者密码异常",
-			"message": "/admin/login",
-		})
 	}
-
 }
