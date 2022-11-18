3249a8c39b1c1ccbfd52053da85ce63b345bac5e
vski5 <2643380185@qq.com>
2022-11-18 05:32:52 +0000

    写完了修改管理员的路由与函数controller，但是html还是有问题

--- controllers/admin/managerController.go
+++ controllers/admin/managerController.go
@@ -20,7 +20,7 @@ func (a ManagerController) Index(c *gin.Context) {
 	})
 }
 
-// 增加商品的页面
+// 增加manager的页面
 func (a ManagerController) Add(c *gin.Context) {
 	//获取所有manager
 	roleList := []models.Role{} //注意，这里是role，不是manager，manager的状态/权限由role表决定
@@ -75,9 +75,75 @@ func (con ManagerController) DoAdd(c *gin.Context) {
 
 }
 
-// 修改商品
-func (a ManagerController) Edit(c *gin.Context) {
-	c.HTML(200, "admin/manager/edit.html", gin.H{})
+// 修改 manager 的页面
+func (con ManagerController) Edit(c *gin.Context) {
+
+	managerList := []models.Manager{}
+	models.DB.Preload("Role").Find(&managerList)
+	id, err := models.Int(c.Query("id"))
+	if err != nil {
+		con.Error(c, "传入数据错误", "/admin/manager/index")
+	} else {
+		// 判断管理是否存在,回传数值
+		managerList := []models.Manager{}
+		//查询对应数值
+		models.DB.Where("id=?", id).Find(&managerList)
+		c.HTML(200, "admin/manager/edit.html", gin.H{
+			"managerList": managerList,
+		})
+	}
+
+}
+
+// 执行 修改manager 的页面
+func (con ManagerController) DoEdit(c *gin.Context) {
+	id, err := models.Int(c.Query("id"))
+	if err != nil {
+		con.Error(c, "传入数据错误", "/admin/manager/index")
+	} else {
+		// 判断管理是否存在,回传数值
+		/* 		managerList := []models.Manager{}
+		   		//查询对应数值
+		   		models.DB.Where("id=?", id).Find(&managerList)
+		   		c.HTML(200, "admin/manager/edit.html", gin.H{
+		   			"managerList": managerList,
+		   		}) */
+
+		username := strings.Trim(c.PostForm("username"), " ")
+		password := strings.Trim(c.PostForm("password"), " ")
+		email := strings.Trim(c.PostForm("email"), " ")
+		mobile, _ := strconv.Atoi(strings.Trim(c.PostForm("mobile"), " "))
+
+		// 用户名和密码长度是否合法
+		if len(username) < 2 || len(password) < 6 {
+			con.Error(c, "用户名或者密码的长度不合法", "/admin/manager/index")
+			return
+		}
+
+		// 判断管理是否存在
+		managerList := []models.Manager{}
+		models.DB.Where("id=?", id).Find(&managerList)
+		if len(managerList) > 0 {
+			// 执行增加管理员
+			manager := models.Manager{
+				Username: username,
+				Password: models.MD5maker(password),
+				Email:    email,
+				Mobile:   mobile,
+				Status:   1,
+				AddTime:  models.GetUnix(),
+			}
+
+			err2 := models.DB.Model(&manager).Select("Username", "Password", "Email", "Mobile", "Status", "AddTime").Updates(manager)
+			if err2 != nil {
+				con.Error(c, "更新管理员失败", "/admin/manager/index")
+				return
+			}
+
+			con.Success(c, "更新管理员成功", "/admin/manager/index")
+		}
+
+	}
 }
 
 // 删除商品
