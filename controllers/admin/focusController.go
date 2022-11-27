5971725ee91849ea0386763a9f256764adb10632
vski5 <2643380185@qq.com>
2022-11-27 13:34:08 +0000

    后台轮播图管理管理系统的主页的控制器

--- controllers/admin/focusController.go
+++ controllers/admin/focusController.go
@@ -1,6 +1,7 @@
 package admin
 
 import (
+	"fake-market/models"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,7 +12,11 @@ type FocusController struct{}
 
 // 后台轮播图管理管理系统的主页
 func (a FocusController) Index(c *gin.Context) {
-	c.HTML(200, "admin/focus/index.html", gin.H{})
+	focusList := []models.Focus{}
+	models.DB.Find(&focusList)
+	c.HTML(http.StatusOK, "admin/focus/index.html", gin.H{
+		"focusList": focusList,
+	})
 }
 
 // 增加轮播图
@@ -28,10 +33,12 @@ func (a FocusController) DoAdd(c *gin.Context) {
 func (a FocusController) Edit(c *gin.Context) {
 	c.HTML(200, "admin/focus/edit.html", gin.H{})
 }
+
 // 执行--修改轮播图
 func (a FocusController) DoEdit(c *gin.Context) {
 	c.HTML(200, "admin/focus/edit.html", gin.H{})
 }
+
 // 删除轮播图
 func (a FocusController) Delete(c *gin.Context) {
 	c.String(http.StatusOK, "test2")
---
+++ models/uploadFile.go
@@ -0,0 +1,8 @@
+package models
+
+//获取上传文件的集合
+
+// 获取上传的图片
+//func UploadImg(c *gin.Context, picName string) (string, error) {
+
+//}
