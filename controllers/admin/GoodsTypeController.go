7a253e59567083590c37334345b6582a20228d67
vski5 <2643380185@qq.com>
2022-12-05 04:44:55 +0000

    执行添加商品类型的操作的控制器

--- controllers/admin/GoodsTypeController.go
+++ controllers/admin/GoodsTypeController.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"fake-market/models"
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -24,7 +25,32 @@ func (con GoodsTypeController) Add(c *gin.Context) {
 }
 
 func (con GoodsTypeController) DoAdd(c *gin.Context) {
-	c.String(200, "test")
+	title := strings.Trim(c.PostForm("title"), " ")
+	description := strings.Trim(c.PostForm("description"), " ")
+	status, err1 := models.Int(c.PostForm("status"))
+
+	if err1 != nil {
+		con.Error(c, "传入的参数不正确", "/admin/goodsType/add")
+		return
+	}
+
+	if title == "" {
+		con.Error(c, "标题不能为空", "/admin/goodsType/add")
+		return
+	}
+	goodsType := models.GoodsType{
+		Title:       title,
+		Description: description,
+		Status:      status,
+		AddTime:     int(models.GetUnix()),
+	}
+
+	err := models.DB.Create(&goodsType).Error
+	if err != nil {
+		con.Error(c, "增加商品类型失败 请重试", "/admin/goodsType/add")
+	} else {
+		con.Success(c, "增加商品类型成功", "/admin/goodsType/index")
+	}
 }
 func (con GoodsTypeController) Edit(c *gin.Context) {
 	c.String(200, "test")
