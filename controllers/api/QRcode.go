12d3989f55584a7b73997f6b1bc397b7295130f2
vski5 <2643380185@qq.com>
2022-12-06 15:35:35 +0000

    URL2Qrcode 返回[]byte字节

--- controllers/api/QRcode.go
+++ controllers/api/QRcode.go
@@ -1,7 +1,7 @@
 package api
 
 import (
-	"fmt"
+	"fake-market/models"
 
 	"github.com/gin-gonic/gin"
 	qrcode "github.com/skip2/go-qrcode"
@@ -10,14 +10,17 @@ import (
 type QRcode struct {
 	url  string
 	size int //size是图像的宽度和高度，单位是像素。
+
+}
+type QRinterface interface {
+	URL2Qrcode()
 }
 
-func (qr QRcode) Qrcode1(c *gin.Context) {
+func (qr QRcode) URL2Qrcode(c *gin.Context) []byte {
 	var png []byte
 	png, err := qrcode.Encode(qr.url, qrcode.Medium, qr.size)
 	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		c.String(200, "返回二维码失败")
+		models.SugarLogger.Errorf("返回二维码失败 : Error = %s", err.Error())
 	}
-	c.String(200, string(png))
+	return png
 }
