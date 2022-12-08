7a26dbedd81be90ae341b762a130e612aae3942d
vski5 <2643380185@qq.com>
2022-12-08 16:18:57 +0000

    半成品：实现jwt.Claims这个接口里的所有方法，将存储JWT的地方变为Redis。JWT的设置模块。JWT的验证模块

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -1,47 +1,70 @@
 package middlewares
 
 import (
-	"fmt"
-	"time"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strings"
 
-	"github.com/golang-jwt/jwt/v4"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9" //连接Redis 7（对应go-redis/v9）
+	jwt "github.com/golang-jwt/jwt/v4"
 )
 
-type JwtBody struct {
-	secret string
-	userId int
+// 为了演示，我们定义一个简单的用户信息结构体
+type User struct {
+	ID        int    `json:"id"`
+	Name      string `json:"name"`
+	LoginName string `json:"login_name"`
 }
 
-func (JwtBody JwtBody) Jwt() {
-	// 存储签名密钥。
-	secret := JwtBody.secret
-
-	// 创建一个新的JWT。
-	token := jwt.New(jwt.SigningMethodHS256)
-
-	// 为该JWT设置一些声明。
-	claims := token.Claims.(jwt.MapClaims) //将JWT的声明转换为一个jwt.MapClaims类型的变量。这样 就可以使用字典语法来设置声明的值，
-	claims["userId"] = JwtBody.userId
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() //设置JWT的过期时间,默认是24H
-
-	// 使用签名密钥对JWT进行签名。
-	tokenString, err := token.SignedString([]byte(secret))
+// 实现jwt.Claims这个接口里的所有方法，将存储JWT的地方变为Redis
+func StoreJWTInRedis(client *redis.Client, token jwt.Token) error {
+	// Convert the claims to JSON
+	claimsJSON, err := json.Marshal(token.Claims)
 	if err != nil {
-		fmt.Println(err)
+		return err
 	}
-	JWT := fmt.Printf("JWT:", tokenString)
 
+	// Store the JSON in Redis
+	err = client.Set(token.Raw, claimsJSON, 0).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
-func JWTCheck() {
-	// 使用签名密钥和JWT验证签名。
-	result, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte(secret), nil
+
+// 设置JWT
+func CreateJWT(c *gin.Context, claims jwt.Claims) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte("mysecret"))
+	if err != nil {
+		http.Error(c.Writer, "Error creating JWT", http.StatusInternalServerError)
+		return
+	}
+	c.Writer.Header().Set("Authorization", "Bearer "+tokenString)
+}
+
+// 验证JWT
+func VerifyJWT(r *http.Request) (jwt.Claims, error) {
+	tokenString := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
+	if tokenString == "" {
+		return nil, errors.New("Missing JWT")
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("Unexpected signing method")
+		}
+		return []byte("mysecret"), nil
 	})
 	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		claims := result.Claims.(jwt.MapClaims)
-		fmt.Println("userId:", claims["userId"])
-		fmt.Println("Expiration Time:", claims["exp"])
+		return nil, err
 	}
+
+	if claims, ok := token.Claims.(jwt.Claims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("Invalid JWT")
 }
