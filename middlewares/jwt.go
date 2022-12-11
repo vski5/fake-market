ff7c6d29b5da255aa5df352e193555f19af07bc5
vski5 <2643380185@qq.com>
2022-12-11 12:55:41 +0000

    与gin结合的jwt验证体系

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -1,70 +1,118 @@
 package middlewares
 
 import (
-	"encoding/json"
 	"errors"
-	"net/http"
-	"strings"
+	"fmt"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis/v9" //连接Redis 7（对应go-redis/v9）
+
 	jwt "github.com/golang-jwt/jwt/v4"
 )
 
-// 为了演示，我们定义一个简单的用户信息结构体
-type User struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	LoginName string `json:"login_name"`
+// 这里定义JWT的Payload部分和加密密码和作用域
+type MyUserJWTClaims struct {
+	//Id       int      `json:"id"`
+	Username string   `json:"username"`
+	Urls     []string `json:"urls"`
+	Secret   []byte   //[]byte("my_secret_key")
+	//Scope    string   //作用域
+	/*
+			jwt.RegisteredClaims这个struct用于存储已注册的声明（claims），并包含了一些公用的声明。
+		Issuer字段存储了声明的发行者（issuer）。
+		Subject字段存储了声明的主题（subject）。
+		Audience字段存储了声明的接收方（audience）。type ClaimStrings []string
+		ExpiresAt字段存储了声明的过期时间（expiration time）.*NumericDate。type NumericDate struct { time.Time }
+		NotBefore字段存储了声明的生效时间（not before）。 type NumericDate struct { time.Time }
+		IssuedAt字段存储了声明的签发时间（issued at）。 type NumericDate struct { time.Time }
+		ID字段存储了声明的唯一标识符（JWT ID）。
+	*/
 }
 
-// 实现jwt.Claims这个接口里的所有方法，将存储JWT的地方变为Redis
-func StoreJWTInRedis(client *redis.Client, token jwt.Token) error {
-	// Convert the claims to JSON
-	claimsJSON, err := json.Marshal(token.Claims)
-	if err != nil {
-		return err
-	}
+// 生成和签名JWT
+func (userJWT MyUserJWTClaims) CreateToken() (string, error) {
 
-	// Store the JSON in Redis
-	err = client.Set(token.Raw, claimsJSON, 0).Err()
-	if err != nil {
-		return err
-	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{ //MapClaims is a claims type that uses the map[string]interface{} for JSON decoding.
+			//"Id":       userJWT.Id,
+			"Username": userJWT.Username,
+			"Urls":     userJWT.Urls,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+	tokenString, err := token.SignedString(userJWT.Secret)
 
-	return nil
+	return tokenString, err
+
 }
 
-// 设置JWT
-func CreateJWT(c *gin.Context, claims jwt.Claims) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString([]byte("mysecret"))
-	if err != nil {
-		http.Error(c.Writer, "Error creating JWT", http.StatusInternalServerError)
-		return
-	}
-	c.Writer.Header().Set("Authorization", "Bearer "+tokenString)
-}
+// 验证 和 解析 JWT
+func ValidateToken(tokenString string, my_secret_key []byte) (back map[string]interface{}, err error) {
 
-// 验证JWT
-func VerifyJWT(r *http.Request) (jwt.Claims, error) {
-	tokenString := strings.TrimPrefix(r.Header.Get("Authorization"), "Bearer ")
-	if tokenString == "" {
-		return nil, errors.New("Missing JWT")
-	}
+	if tokenString == " " { //如果没有Token
 
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("Unexpected signing method")
+		//c.String(http.StatusInternalServerError, err.Error())
+		return nil, errors.New("token error")
+	} else { //token和 JWT 装饰器函数传递到 parse 方法中，然后返回接口和错误
+		token, _ := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return fmt.Errorf("there was an error in parsing"), nil //错误消息首字母要小写。创建一个 error错误。
+			}
+			return my_secret_key, nil //[]byte("my_secret_key")记得改成自己的密码，用struct解决。
+		})
+		if token == nil {
+			fmt.Println("invalid token")
+		} else {
+			// 解析JWT
+			claims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				fmt.Println("couldn't parse claims")
+				return nil, errors.New("token error")
+			} else {
+				// 以MyUserJWTClaims构建的JWT为例
+				//id := claims["Id"]
+				username := claims["Username"]
+				urls := claims["Urls"]
+
+				userJWTS := map[string]interface{}{
+					//"Id":       id,
+					"Username": username,
+					"Urls":     urls,
+				}
+				return userJWTS, errors.New("token error")
+			}
+
 		}
-		return []byte("mysecret"), nil
-	})
-	if err != nil {
-		return nil, err
-	}
 
-	if claims, ok := token.Claims.(jwt.Claims); ok && token.Valid {
-		return claims, nil
 	}
-	return nil, errors.New("Invalid JWT")
+	return nil, errors.New("validate token error")
+
+}
+
+// 将使用GIN设置JWT 和 生成和签名JWT 封装到一起。 Scope是 作用域
+func (userJWT MyUserJWTClaims) SetJWT(c *gin.Context, Scope string) {
+	tokenString, err := userJWT.CreateToken()
+	if tokenString != "" && err == nil {
+		c.SetCookie("Token", tokenString, 3600, "/", Scope, false, true)
+	}
+}
+
+// 将使用GIN获取JWT 和 验证并且解析JWT 封装到一起。需要实例化MyUserJWTClaims里的Secret
+func (userJWT MyUserJWTClaims) GetJWT(c *gin.Context) ([]string, error) {
+	// 从请求头中获取 Token 信息
+	tokenget111, _ := c.Request.Cookie("Token")
+
+	ca, _ := ValidateToken(tokenget111.Value, userJWT.Secret)
+
+	if urls, ok := ca["Urls"].([]interface{}); ok {
+		y := make([]string, len(urls))
+		for i, v := range urls {
+			y[i] = v.(string)
+		}
+		// y 是 []string 类型
+		return y, nil
+
+	} else {
+		// x["Urls"] 不是 []interface{} 类型
+		return nil, errors.New("x[\"Urls\"] 不是 []interface{} 类型")
+	}
 }
