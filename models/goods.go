51d95617972f8e8ccda9952c24cf6468f05366b1
vski5 <2643380185@qq.com>
2022-12-13 13:25:35 +0000

    添加包含商品价格等商品信息的ORM的struct--》Goods

--- models/goods.go
+++ models/goods.go
@@ -47,3 +47,38 @@ type GoodsTypeAttribute struct {
 func (GoodsTypeAttribute) TableName() string {
 	return "goods_type_attribute"
 }
+
+
+type Goods struct {
+	Id            int
+	Title         string
+	SubTitle      string
+	GoodsSn       string
+	CateId        int
+	ClickCount    int
+	GoodsNumber   int
+	Price         float64
+	MarketPrice   float64
+	RelationGoods string
+	GoodsAttr     string
+	GoodsVersion  string
+	GoodsImg      string
+	GoodsGift     string
+	GoodsFitting  string
+	GoodsColor    string
+	GoodsKeywords string
+	GoodsDesc     string
+	Content       string
+	IsDelete      int
+	IsHot         int
+	IsBest        int
+	IsNew         int
+	GoodsTypeId   int
+	Sort          int
+	Status        int
+	AddTime       int
+}
+
+func (Goods) TableName() string {
+	return "goods"
+}
