47b0219688a627b3c93e89613c6de7032c92119e
vski5 <2643380185@qq.com>
2022-12-13 14:13:51 +0000

    实现了TAB标签页的goodsinfo

--- controllers/admin/GoodsInfoController.go
+++ controllers/admin/GoodsInfoController.go
@@ -1,16 +1,20 @@
 package admin
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type GoodsInfoController struct {
 	BaseController
 }
 
 func (con GoodsInfoController) Index(c *gin.Context) {
-
+	c.HTML(http.StatusOK, "admin/goods/index.html", gin.H{})
 }
 func (con GoodsInfoController) Add(c *gin.Context) {
-
+	c.HTML(http.StatusOK, "admin/goods/add.html", gin.H{})
 }
 func (con GoodsInfoController) DoAdd(c *gin.Context) {
 
