6c4bef87089f4833013b2a0deb869ae86c5f41ae
vski5 <2643380185@qq.com>
2023-01-12 06:35:40 +0000

    修补goods对应的ORM，之前少写了一个列

--- models/goods.go
+++ models/goods.go
@@ -67,7 +67,7 @@ type Goods struct {
 	GoodsColor    string
 	GoodsKeywords string
 	GoodsDesc     string
-	Content       string
+	GoodsContent  string
 	IsDelete      int
 	IsHot         int
 	IsBest        int
