56e90f0fa99be06a9615e2290fcd156efe0d457b
vski5 <2643380185@qq.com>
2023-05-15 14:53:59 +0100

    setting的orm操作部分，没有写路由

--- controllers/admin/setting.go
+++ controllers/admin/setting.go
@@ -1,5 +1,8 @@
 package admin
+
 import (
+	"fake-market/models"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,6 +12,39 @@ type GoodsSettingController struct {
 	BaseController
 }
 
-func (con GoodsSettingController) Index(c *gin.Context){
-	c.HTML(http.StatusOK, "admin/setting/index.html", gin.H{})
+func (con GoodsSettingController) Index(c *gin.Context) {
+	setting := models.Setting{}
+	models.DB.First(&setting)
+	c.HTML(http.StatusOK, "admin/setting/index.html", gin.H{
+		"setting": setting,
+	})
+}
+
+func (con GoodsSettingController) DoEdit(c *gin.Context) {
+	setting := models.Setting{Id: 1}
+	models.DB.Find(&setting)
+	if err := c.ShouldBind(&setting); err != nil {
+		fmt.Println(err)
+		con.Error(c, "修改数据失败,请重试", "/admin/setting/index")
+		return
+	} else {
+		// 上传图片 logo
+		siteLogo, err1 := models.UploadImg(c, "site_logo")
+		if len(siteLogo) > 0 && err1 == nil {
+			setting.SiteLogo = siteLogo
+		}
+		//上传图片 no_picture
+		noPicture, err2 := models.UploadImg(c, "no_picture")
+		if len(noPicture) > 0 && err2 == nil {
+			setting.NoPicture = noPicture
+		}
+
+		err3 := models.DB.Save(&setting).Error
+		if err3 != nil {
+			con.Error(c, "修改数据失败", "/admin/setting/index")
+			return
+		}
+
+		con.Success(c, "修改数据成功", "/admin/setting/index")
+	}
 }
